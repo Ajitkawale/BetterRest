abedcac9f94d22d546e968b18474ab7243fdf6c8
Ajit Kawale <ajitkawale27@gmail.com>
2025-09-28 13:36:16 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by AJit Kawale on 28/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
